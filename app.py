925097b3e53085dec77dd73d244dceb310f16b35
Tirtha <tirthas970@gmail.com>
2026-06-28 10:25:31 -0400

    converted the data script to a class; GUI table has been setup

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 from datetime import timedelta, date
 import requests
 import json
+from AsteroidData import CollectAsteroidData
 
-###TO DO: MAKE INTO A FUNCTION
+
 
 st.title("AeroTrack Prime")
 
@@ -12,52 +13,13 @@ API_KEY = st.secrets["nasa_key"]
 today = date.today()
 tomorrow = today + timedelta(days=1)
 
-print(f"Tomorrow: {tomorrow}")
+collect_asteroid_data = CollectAsteroidData(API_KEY, today, tomorrow)
+
+asteroid_data = collect_asteroid_data.get_data()
+
+#st.subheader("1. Interactive Table (st.dataframe)")
+#st.dataframe(asteroid_data, use_container_width=True)
 
 
-query_params = {
-    "api_key": API_KEY,
-    "start_date": today,
-    "end_date": tomorrow
-}
-
-URL = "https://api.nasa.gov/neo/rest/v1/feed"
-
-response = requests.get(URL, params=query_params)
-
-if response.status_code == 200:
-    data = response.json()
-
-    neo = data["near_earth_objects"]
-
-    for date, asteroids_info in neo.items():
-
-        print(f"Asteriods of {date}")
-        
-        for asteroid in asteroids_info: 
-            name = asteroid["name"]
-            
-            #size
-            diameter = asteroid[ "estimated_diameter"]
-            diameter_meters = diameter["meters"]
-            meters_max = diameter_meters["estimated_diameter_max"]
-
-            #speeds
-            approach_data = asteroid["close_approach_data"]
-            asteroid_approahc_facts = approach_data[0]
-            relative_velocity = asteroid_approahc_facts["relative_velocity"]
-            velocity_mph = relative_velocity["miles_per_hour"]
-
-            is_hazardous = asteroid["is_potentially_hazardous_asteroid"]
-          
-
-            print(f"Asteroid Name: {name} | Hazardous: {is_hazardous} | Size: {meters_max} | Speed: {velocity_mph}")
-
-
-
-else:
-    print(f"NOT WORKING  {response.status_code}")
-
-
-
-
+st.subheader("2. Static Table (st.table)")
+st.table(asteroid_data)
--- asteroidData.py
+++ asteroidData.py
@@ -1,5 +1,80 @@
 import requests
-import time
-import streamlit as st
+import requests
+import pandas as pd
 
-api_key = st.secret["nasa_key"]
+class CollectAsteroidData:
+    def __init__(self, API_KEY, today, tomorrow):
+        self.query_params = {
+            "api_key": API_KEY,
+            "start_date": today,
+            "end_date": tomorrow
+
+        }
+
+    def get_data(self):
+
+        name_list = []
+        size_list = []
+        speed_list = []
+        is_hazardous_list= []
+
+        URL = "https://api.nasa.gov/neo/rest/v1/feed"
+        response = requests.get(URL, params=self.query_params)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            neo = data["near_earth_objects"]
+
+            for date, asteroids_info in neo.items():
+
+                print(f"Asteriods of {date}")
+                
+                for asteroid in asteroids_info: 
+                    name = asteroid["name"]
+
+                    #name is formatted like this: (name)
+                    #remove the ()
+
+                    clean_name = name.replace("(", "").replace(")", "")
+                           
+                    #size
+                    diameter = asteroid[ "estimated_diameter"]
+                    diameter_meters = diameter["meters"]
+                    meters_max = diameter_meters["estimated_diameter_max"]
+
+                    #speeds
+                    approach_data = asteroid["close_approach_data"]
+                    asteroid_approahc_facts = approach_data[0]
+                    relative_velocity = asteroid_approahc_facts["relative_velocity"]
+                    velocity_mph = relative_velocity["miles_per_hour"]
+
+                    is_hazardous = asteroid["is_potentially_hazardous_asteroid"]
+                
+                    name_list.append(clean_name)
+                    size_list.append(meters_max)
+                    speed_list.append(velocity_mph)
+                    is_hazardous_list.append(is_hazardous)
+
+                    print(f"Asteroid Name: {name} | Hazardous: {is_hazardous} | Size: {meters_max} | Speed: {velocity_mph}") #testing
+
+
+            asteroid_data = pd.DataFrame(
+                {
+                    "Name": name_list,
+                    "Size (meters)": size_list,
+                    "speed_list (mph)": speed_list,
+                    "Is Hazardous": is_hazardous_list,
+
+                }
+            )
+
+            return asteroid_data
+                    
+
+
+
+        else:
+            print(f"NOT WORKING  {response.status_code}")
+
+
